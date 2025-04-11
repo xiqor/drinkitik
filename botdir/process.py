c414628887c68f07a1a884e27208c6890e512ab4
Max F <xiqor@list.ru>
2025-04-11 18:59:14 +0300

    imporved category handling and readability

--- botdir/process.py
+++ botdir/process.py
@@ -5,53 +5,25 @@
 #   1.3) detect category
 #   1.4) sum it up by product and category
 # 2) create ultra dictionary or db for naming
+# 3) make this dic not hardcoded, easy to manage from outside
 
 # for now it's ok, but in future make it NOT hardcoded, so you can add new keys not througn code,
 # but from outside
 # DON'T FORGET TO LOWER STRING BEFORE COMPARING TO SYNONYMS
-naming = {
-    'Мбн': ['мбн',
-            'молоко банан', 'банан молоко',
-            'молоко банановое', 'банановое молоко',
-            'молоко бн', 'бн молоко',
-            'м банан', 'банан м',
-            'м банановое', "банановое м"],
-    'Мбз': ['мбз', 'безлактоза', 'безлактоза',
-            'молоко безлактоза', 'безлактоза молоко',
-            'молоко безлактозное', 'безлактозное молоко',
-            'молоко бз', 'бз молоко',
-            'м безлактоза', 'безлактоза м',
-            'м безлактозное', "безлактозное м"],
-    'Мкк': ['мкк',
-            'молоко кокос', 'кокос молоко',
-            'молоко кокосовое', 'кокосовое молоко',
-            'молоко кк', 'кк молоко',
-            'м кокос', 'кокос м',
-            'м кокосовое', 'кокосовое м'],
-    'Мк': ['мк', 'корова',
-           'молоко корова', 'корова молоко',
-           'молоко коровье', 'коровье молоко',
-           'молоко к', 'к молоко',
-           'м коровье', 'коровье м',
-           'м корова', 'корова м'],
-    'Ммд': ['ммд', 'миндаль', 'миндальное',
-           'молоко миндаль', 'миндаль молоко',
-           'молоко миндальное', 'миндальное молоко',
-           'молоко мд', 'мд молоко',
-           'м миндаль', 'миндаль м',
-           'м миндальное', 'миндальное м'],
-    'Мо': ['мо', 'овсяное',
-           'молоко овсянка', 'овсянка молоко',
-           'молоко овсяное', 'овсяное молоко',
-           'молоко о', 'о молоко',
-           'м овсяное', 'овсяное м',
-           'м овсянка', 'овсянка м'],
-}
+from botdir.dictionaries import names, categories
 
-synonym_to_main = {} # reverse dic used to search synonyms
-for main_name, synonym_list in naming.items():
-        for synonym in synonym_list:
+
+def reverse_dic(dic):
+    synonym_to_main = {} # reverse dic used to search synonyms
+    for main_name, synonym_list in dic.items():
+       for synonym in synonym_list:
             synonym_to_main[synonym] = main_name
+    return synonym_to_main
+
+
+names = reverse_dic(names)
+categories = reverse_dic(categories)
+
 
 def process_messages(messages: list[str]):
     # Твоя логика анализа сообщений
@@ -59,7 +31,7 @@ def process_messages(messages: list[str]):
     total = {}
     for message in messages:
 
-        # change it later how to handle when product is more than two words
+        # change it later how to handle when you have multiple products in one message
         split_message = message.split(' ')
         if len(split_message) != 3:
             continue
@@ -67,10 +39,14 @@ def process_messages(messages: list[str]):
 
         # normalizing name
         product_name = product_name.lower()
-        normalized_name = synonym_to_main.get(product_name, product_name)
+        normalized_name = names.get(product_name, product_name)
+
+        # normalizing categories
+        category = category.lower()
+        normalized_category = categories.get(category, category)
 
         # search
-        key = (normalized_name, category) # tuple like (мк, порча)
+        key = (normalized_name, normalized_category) # tuple like (мк, порча)
         if key in total: # check if key already exists
             total[key] += int(product_amount) # maybe later change to float, bc some shi is float
         else:
